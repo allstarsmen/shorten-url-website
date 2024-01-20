b7284aed89e99d038c5268a97609623d82c29e0f
Nam Pham <allstarsmen@gmail.com>
2024-01-20 08:32:21 +0700

    Insert the new url into database

--- app/actions.ts
+++ app/actions.ts
@@ -3,6 +3,7 @@
 import { generateUniqueString } from "@/lib/string";
 import { z } from "zod";
 import { InitState } from "./types/initState";
+import clientPromise from "@/lib/db";
 
 export async function generateURL(prevState: InitState, formData: FormData) {
   const schema = z
@@ -23,10 +24,24 @@ export async function generateURL(prevState: InitState, formData: FormData) {
   }
 
   const data = parse.data;
+  const client = await clientPromise;
+  const db = client.db("TEST");
+  const uniqueStr = generateUniqueString();
+  const result = await db.collection("shortened_urls").insertOne({
+    originUrl: data.originUrl,
+    uniqueStr,
+  });
+
+  if (!result.acknowledged) {
+    return {
+      success: false,
+      message: "Failed to generate new URL",
+    };
+  }
 
   return {
     success: true,
-    uniqueStr: generateUniqueString(),
+    uniqueStr,
     message: "New url is generated successfully",
   };
 }
