5eb728c79bf1a57b38c5fa8b4d289dd38630f60c
Nam Pham <allstarsmen@gmail.com>
2024-01-20 10:19:44 +0700

    add more test cases

--- __tests__/url-lib.test.tsx
+++ __tests__/url-lib.test.tsx
@@ -7,4 +7,10 @@ describe("Shortened URL", () => {
   it("Returns FALSE when receiving invalid string", () => {
     expect(isValidURL("x1QLX87&")).toBe(false);
   });
+  it("Returns FALSE when receiving valid string but the length less than 8 characters", () => {
+    expect(isValidURL("x1QLX8")).toBe(false);
+  });
+  it("Returns FALSE when receiving valid string but the length longer than 8 characters", () => {
+    expect(isValidURL("x1QLX87Pfr")).toBe(false);
+  });
 });
