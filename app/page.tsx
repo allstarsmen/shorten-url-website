5a6f16c892dda3664fbed4eb95600b1897cb1eeb
Nam Pham <allstarsmen@gmail.com>
2024-01-20 09:49:06 +0700

    Create a toasts component

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { useFormState, useFormStatus } from "react-dom";
 import { generateURL } from "./actions";
 import { InitState } from "./types/initState";
+import Toasts from "./toasts";
 
 const initState: InitState = {
   message: "",
@@ -52,48 +53,50 @@ export default function Home() {
   }, [isCopied]);
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-10">
-      <h1 className="text-3xl">Best free tool for shortening URLs</h1>
-      <form
-        action={formAction}
-        className="flex w-full gap-5 items-baseline mt-5"
-      >
-        <input
-          type="text"
-          name="originUrl"
-          className="flex-grow mt-5 border border-gray-500 rounded px-3 py-1"
-          placeholder="Enter your url here"
-          required
-        />
-        <SubmitButton />
-      </form>
-      {!state.success && state.message && (
-        <div className="w-full mt-1 text-red-500">{state.message}</div>
-      )}
-      {state.success && (
-        <>
-          <div className="mt-3 w-full text-xl font-bold">Result:</div>
-          <div className="mt-2 w-full px-3 py-3 border flex items-baseline gap-5">
-            <div className="flex-grow">
-              <a
-                href={newURL(state.uniqueStr)}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-700 hover:text-blue-900"
+    <>
+      <main className="flex min-h-screen flex-col items-center p-10">
+        <h1 className="text-3xl">Best free tool for shortening URLs</h1>
+        <form
+          action={formAction}
+          className="flex w-full gap-5 items-baseline mt-5"
+        >
+          <input
+            type="text"
+            name="originUrl"
+            className="flex-grow mt-5 border border-gray-500 rounded px-3 py-1"
+            placeholder="Enter your url here"
+            required
+          />
+          <SubmitButton />
+        </form>
+        {!state.success && state.message && (
+          <div className="w-full mt-1 text-red-500">{state.message}</div>
+        )}
+        {state.success && (
+          <>
+            <div className="mt-3 w-full text-xl font-bold">Result:</div>
+            <div className="mt-2 w-full px-3 py-3 border flex items-baseline gap-5">
+              <div className="flex-grow">
+                <a
+                  href={newURL(state.uniqueStr)}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-700 hover:text-blue-900"
+                >
+                  {newURL(state.uniqueStr)}
+                </a>
+              </div>
+              <button
+                className="border px-3 py-1 bg-blue-500 text-white hover:bg-blue-700"
+                onClick={handleOnCopy}
               >
-                {newURL(state.uniqueStr)}
-              </a>
+                Copy
+              </button>
             </div>
-            <button
-              className="border px-3 py-1 bg-blue-500 text-white hover:bg-blue-700"
-              onClick={handleOnCopy}
-            >
-              Copy
-            </button>
-          </div>
-          {isCopied && <div className="w-full mt-1">Copied to clipboard</div>}
-        </>
-      )}
-    </main>
+          </>
+        )}
+      </main>
+      {isCopied && <Toasts />}
+    </>
   );
 }
---
+++ app/toasts.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Toasts() {
+  return (
+    <div className="absolute top-5 right-5 bg-green-600 text-white px-3 py-1 rounded shadow-2xl">
+      Copied to clipboard
+    </div>
+  );
+}
